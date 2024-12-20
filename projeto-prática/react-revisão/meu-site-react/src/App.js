a6bdf157ef2f4550b5de0c9d947a926816bdb032
HectorG9 <hector.guedesmonteiro@gmail.com>
2024-12-20 16:41:40 -0300

    commitando todos os projetos

---
+++ projeto-prática/react-revisão/meu-site-react/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css'; // Estilos que você pode customizar
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bem-vindo ao meu site em React!</h1>
+        <p>Este é um site básico feito com React.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Saiba mais sobre o React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
